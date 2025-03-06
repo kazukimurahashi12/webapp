ad379722400cbbc3ca012a30e3487e1c00c00e9f
kazukimurahashi12 <unchain1ed@gmail.com>
2025-03-06 13:35:48 +0900

    Fix MockUnitTest #2

--- server-app/domain/session.go
+++ server-app/domain/session.go
@@ -1,6 +1,13 @@
 package domain
 
+import "github.com/go-playground/validator/v10"
+
 type Session struct {
-	UserID string
-	Token  string
+	UserID string `validate:"required,uuid4"`
+	Token  string `validate:"required,min=32"`
+}
+
+func (s *Session) Validate() error {
+	validate := validator.New()
+	return validate.Struct(s)
 }
